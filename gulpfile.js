010f129d35b15070f58b3df8a6886332aac3541a
Yoan <ydelmaire@hotmail.com>
2018-08-10 16:28:20 +0200

    Share work

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    cleanCSS = require('gulp-clean-css')
+    publicPath = './static/'
+;
+
+gulp.task('sass', () => {
+    return sass('static/scss/style.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('static/css'))
+});
+
+gulp.task('compressCSS', ['sass'], () => {
+    return gulp.src('static/css/*.css')
+        .pipe(cleanCSS({compatibility:'ie8'}))
+        .pipe(gulp.dest('static/css'))
+});
+
+gulp.task('default', ['compressCSS'], () => {});
+
+gulp.task('watch', () => {
+    gulp.watch(publicPath + 'scss/*.scss', ['compressCSS']);
+});
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'index.apps.IndexConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR + '/templates/',
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
